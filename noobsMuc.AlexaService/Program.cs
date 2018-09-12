a042654bc3b080d7e540981ac98e2ed7ac7672b3
Dominik Lindemann <dominik.lindemann@hotmail.de>
2018-09-12 14:04:42 +0200

    remove duplicate log4net registration

--- noobsMuc.AlexaService/Program.cs
+++ noobsMuc.AlexaService/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace noobsMuc.AlexaService
 {
@@ -20,14 +13,6 @@ namespace noobsMuc.AlexaService
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    // The ILoggingBuilder minimum level determines the
-                    // the lowest possible level for logging. The log4net
-                    // level then sets the level that we actually log at.
-                    logging.AddLog4Net();
-                    logging.SetMinimumLevel(LogLevel.Debug);
-                })
                 .Build();
     }
 }
